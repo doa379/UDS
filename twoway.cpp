83f0cf3163429970ec0e0a3e17ccf963d6981811
doa379 <doa379@gmail.com>
2020-08-03 00:12:05 +0100

    ;

--- twoway.cpp
+++ twoway.cpp
@@ -11,26 +11,42 @@
 #include <thread>
 #include <cstdint>
 #include <csignal>
-#define SOCKET_PATH "/tmp/"
+#define SOCKET_PATH "/tmp"
 static bool quit;
 
 class Uds
 {
+protected:
   struct sockaddr_un addr;
   int32_t fd, rc, cl;
-  std::string socket_path, buf;
+  std::string socket_path;
 
   public:
-  Uds(std::string);
+  Uds(void);
   ~Uds(void);
-  bool writer(std::string);
-  bool writer_connector(void);
-  void reader(void);
 };
 
-Uds::Uds(std::string name)
+class Reader : public Uds
 {
-  socket_path = SOCKET_PATH + name;
+public:
+  Reader(void) { unlink(socket_path.c_str()); };
+  ~Reader(void) { close(cl); };
+  void r(void);
+};
+
+class Writer : public Uds
+{
+public:
+  bool w(void *, size_t);
+  bool conn(void);
+};
+
+static Writer prod;
+static Reader cons;
+
+Uds::Uds(void)
+{
+  socket_path = static_cast<std::string>(SOCKET_PATH) + "/" + "twoway_socket";
 
   if ((fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1)
   {
@@ -48,7 +64,7 @@ Uds::~Uds(void)
 
 }
 
-bool Uds::writer_connector(void)
+bool Writer::conn(void)
 {
   if (connect(fd, (struct sockaddr *) &addr, sizeof(addr)) == -1)
     return 0;
@@ -56,9 +72,9 @@ bool Uds::writer_connector(void)
   return 1;
 }
 
-bool Uds::writer(std::string buf)
+bool Writer::w(void *data, size_t size)
 {
-  if ((rc = write(fd, buf.c_str(), buf.size() * sizeof(char))) != buf.size())
+  if ((rc = write(fd, data, size)) != size)
   {
     if (rc > 0)
       fprintf(stderr,"partial write");
@@ -73,22 +89,21 @@ bool Uds::writer(std::string buf)
   return 1;
 }
 
-void Uds::reader(void)
+void Reader::r(void)
 {
-  unlink(socket_path.c_str());
-
   if (bind(fd, (struct sockaddr *) &addr, sizeof(addr)) == -1)
   {
     perror("bind error");
-    exit(-1);
+    return;
   }
 
   if (listen(fd, 5) == -1)
   {
     perror("listen error");
-    exit(-1);
+    return;
   }
 
+  void *data = malloc(100);
   while (!quit)
   {
     if ((cl = accept(fd, NULL, NULL)) == -1)
@@ -96,57 +111,30 @@ void Uds::reader(void)
       perror("accept error");
       continue;
     }
-
-    while ((rc = read(cl, &buf[0], sizeof(buf))) > 0 && !quit)
-    {
-      //buf[rc] = 0x00;
-      printf("read %u bytes: %*s\n", rc, rc, buf.c_str());
-    }
+  
+    while ((rc = read(cl, data, sizeof(data))) > 0 && ((char *) data)[0] != '\0')
+      printf("read %u bytes: %s\n", rc, (char *) data);
 
     if (rc == -1)
     {
       perror("read");
-      exit(-1);
+      break;
     }
 
     else if (rc == 0)
     {
       printf("EOF\n");
-      close(cl);
+      break;
     }
   }
 
-  close(cl);
-  printf("Reader exit\n");
-}
-
-void writer_th(void)
-{
-  Uds w("name");
-  // Send events
-  srand(0);
-
-  while (!w.writer_connector())
-    sleep(1);
-
-  while (!quit)
-  {
-    unsigned event = rand() % 100;
-
-    if (w.writer(std::to_string(event)))
-      printf("Wrote %u\n", event);
-
-    sleep(5);
-  }
-
-  w.writer("0x00");
-  printf("Writer exit\n");
+  printf("Consumer/Reader exit\n");
+  free(data);
 }
 
 void reader_th(void)
 {
-  Uds r("name");
-  r.reader();
+  cons.r();
 }
 
 void signal_handler(int signum)
@@ -158,9 +146,27 @@ void signal_handler(int signum)
 int main()
 {
   signal(SIGINT, signal_handler);
-  std::thread writer(writer_th);
   std::thread reader(reader_th);
-  writer.join();
+
+  while (!prod.conn())
+    sleep(1);
+
+  srand(0);
+  void *data = malloc(100);
+  while (!quit)
+  {
+    unsigned event = rand() % 100;
+    memcpy(data, std::to_string(event).c_str(), sizeof(data));
+
+    if (prod.w(data, sizeof(data)))
+      printf("Wrote event %d\n", event);
+
+    sleep(1);
+  }
+  printf("Producer/Writer exit\n");
+  memcpy(data, "\0", 2);
+  prod.w(data, 4);
   reader.join();
+  free(data);
   return 0;
 }
