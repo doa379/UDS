c219c572c2ed457ae402b7c1db51dcbdbe323bfa
doa379 <doa379@gmail.com>
2020-08-04 19:54:06 +0100

    ;

---
+++ twoway.c
@@ -0,0 +1,161 @@
+// UNIX Domain Sockets
+// $ gcc twoway.cpp -o twoway -l pthread && ./twoway
+
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <stdint.h>
+#include <signal.h>
+#include <stdbool.h>
+#define SOCKET_PATH "/tmp"
+#define PATH_LEN 128
+static bool quit;
+
+typedef struct
+{
+  struct sockaddr_un addr;
+  int32_t fd, rc, cl;
+  char socket_path[PATH_LEN];
+} uds_t;
+
+void deinit_uds(uds_t *uds)
+{
+  free(uds);
+}
+
+uds_t *init_uds(void)
+{
+  uds_t *uds = calloc(1, sizeof *uds);
+  sprintf(uds->socket_path, "%s/$s", SOCKET_PATH, "twoway_socket");
+
+  if ((uds->fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1)
+  {
+    perror("socket error");
+    deinit_uds(uds);
+    exit(-1);
+  }
+
+  memset(&uds->addr, 0, sizeof(uds->addr));
+  uds->addr.sun_family = AF_UNIX;
+  strncpy(uds->addr.sun_path, uds->socket_path, sizeof(uds->addr.sun_path) - 1);
+  unlink(uds->socket_path);
+  return uds;
+}
+
+bool connector(uds_t *uds)
+{
+  if (connect(uds->fd, (struct sockaddr *) &uds->addr, sizeof(uds->addr)) == -1)
+    return 0;
+
+  return 1;
+}
+
+bool sender(uds_t *uds, void **data)
+{
+  if ((uds->rc = write(uds->fd, data, 8)) != 8)
+  {
+    if (uds->rc > 0)
+      fprintf(stderr,"partial write");
+
+    else
+    {
+      perror("write error");
+      return 0;
+    }
+  }
+
+  return 1;
+}
+
+void *receiver(void *userp)
+{
+  uds_t *uds = userp;
+  
+  if (bind(uds->fd, (struct sockaddr *) &uds->addr, sizeof(uds->addr)) == -1)
+  {
+    perror("bind error");
+    return NULL;
+  }
+
+  if (listen(uds->fd, 5) == -1)
+  {
+    perror("listen error");
+    return NULL;
+  }
+
+  int64_t data;
+  while (!quit)
+  {
+    if ((uds->cl = accept(uds->fd, NULL, NULL)) == -1)
+    {
+      perror("accept error");
+      continue;
+    }
+  
+    while ((uds->rc = read(uds->cl, &data, 8)) > 0)
+    {
+      printf("read %u bytes: %s\n", uds->rc, (char *) data);
+      sleep(2);
+      printf("read finished on %s\n", (char *) data);
+      free((void *) data);
+    }
+
+    if (uds->rc == -1)
+    {
+      perror("read");
+      break;
+    }
+
+    else if (uds->rc == 0)
+    {
+      printf("EOF\n");
+      break;
+    }
+  }
+
+  //if ((void *) data)
+  //free((void *) data);
+  printf("Consumer/Reader exit\n");
+  return NULL;
+}
+
+void signal_handler(int signum)
+{
+  printf("\nExit...\n");
+  quit = 1;
+}
+
+int main()
+{
+  signal(SIGINT, signal_handler);
+  uds_t *prod = init_uds(), *cons = init_uds();
+  pthread_t reader_th;
+  pthread_create(&reader_th, NULL, &receiver, cons);
+
+  while (!connector(prod))
+    sleep(1);
+
+  srand(0);
+  
+  while (!quit)
+  {
+    unsigned event = rand() % 100;
+    void *data = malloc(sizeof(2 * sizeof(char)));
+    sprintf((char *) data, "%d", event);
+
+    if (sender(prod, &data))
+      printf("Wrote event %d\n", event);
+
+    sleep(1);
+  }
+  printf("Producer/Writer exit\n");
+  close(cons->cl);
+  pthread_join(reader_th, NULL);
+  deinit_uds(prod);
+  deinit_uds(cons);
+  return 0;
+}
--- twoway.cpp
+++ twoway.cpp
@@ -13,7 +13,13 @@
 #include <csignal>
 #define SOCKET_PATH "/tmp"
 static bool quit;
-
+/*
+struct Data
+{
+  void *data;
+  size_t size;
+};
+*/
 class Uds
 {
 protected:
@@ -30,14 +36,16 @@ class Reader : public Uds
 {
 public:
   Reader(void) { unlink(socket_path.c_str()); };
-  ~Reader(void) { close(cl); };
+  //~Reader(void) { close(cl); };
+  void close_socket(void) { close(cl); };
   void r(void);
 };
 
 class Writer : public Uds
 {
 public:
-  bool w(void *, size_t);
+  //bool w(void *, size_t);
+  bool w(void **);
   bool conn(void);
 };
 
@@ -71,10 +79,27 @@ bool Writer::conn(void)
 
   return 1;
 }
-
+/*
 bool Writer::w(void *data, size_t size)
 {
   if ((rc = write(fd, data, size)) != size)
+  {
+    if (rc > 0)
+      fprintf(stderr,"partial write");
+
+    else
+    {
+      perror("write error");
+      return 0;
+    }
+  }
+
+  return 1;
+}
+*/
+bool Writer::w(void **data)
+{
+  if ((rc = write(fd, data, 8)) != 8)
   {
     if (rc > 0)
       fprintf(stderr,"partial write");
@@ -102,7 +127,7 @@ void Reader::r(void)
     perror("listen error");
     return;
   }
-
+/*
   void *data = malloc(100);
   while (!quit)
   {
@@ -130,6 +155,40 @@ void Reader::r(void)
 
   printf("Consumer/Reader exit\n");
   free(data);
+  */
+  int64_t data;
+  while (!quit)
+  {
+    if ((cl = accept(fd, NULL, NULL)) == -1)
+    {
+      perror("accept error");
+      continue;
+    }
+  
+    while ((rc = read(cl, &data, 8)) > 0)
+    {
+      printf("read %u bytes: %s\n", rc, (char *) data);
+      sleep(2);
+      printf("read finished on %s\n", (char *) data);
+      free((void *) data);
+    }
+
+    if (rc == -1)
+    {
+      perror("read");
+      break;
+    }
+
+    else if (rc == 0)
+    {
+      printf("EOF\n");
+      break;
+    }
+  }
+
+  //if ((void *) data)
+    //free((void *) data);
+  printf("Consumer/Reader exit\n");
 }
 
 void reader_th(void)
@@ -152,6 +211,7 @@ int main()
     sleep(1);
 
   srand(0);
+  /*
   void *data = malloc(100);
   while (!quit)
   {
@@ -165,8 +225,24 @@ int main()
   }
   printf("Producer/Writer exit\n");
   memcpy(data, "\0", 2);
-  prod.w(data, 4);
+  prod.w(data, 2);
   reader.join();
   free(data);
+  */
+  
+  while (!quit)
+  {
+    unsigned event = rand() % 100;
+    void *data = malloc(sizeof(2 * sizeof(char)));
+    memcpy(data, std::to_string(event).c_str(), 2 * sizeof(char));
+
+    if (prod.w(&data))
+      printf("Wrote event %d\n", event);
+
+    sleep(1);
+  }
+  printf("Producer/Writer exit\n");
+  cons.close_socket();
+  reader.join();
   return 0;
 }
